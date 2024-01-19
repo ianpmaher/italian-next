fed3f71683f14f02101e68139e4038af4f591f36
Ian Maher <ianpmaher@gmail.com>
2024-01-19 10:23:06 -0500

    nvm just tailwind for now

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => (
+    <header className="flex flex-col items-center justify-center">
+        <h1>Learn Italian</h1>
+        <h2>🇮🇹🇬🇧</h2>
+    </header>
+);
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "custom-pink": "#EF476F",
+                "custom-yellow": "#FFD166",
+                "custom-green": "#06D6A0",
+                "custom-blue": "#118AB2",
+                "custom-dark-blue": "#073B4C",
+            },
+            backgroundImage: {
+                "custom-gradient": "background: linear-gradient(90deg, var(--green-color) 0%, var(--blue-color) 100%)"
+            }     
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
